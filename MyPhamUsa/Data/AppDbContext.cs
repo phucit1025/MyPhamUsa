d73661c1b960678c26f4b4ddeec1eea47aaf9b62
Phuc Tran <phucit1025@gmail.com>
2019-01-21 20:06:48 +0700

    Config Project

---
+++ MyPhamUsa/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPhamUsa.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext() : base((new DbContextOptionsBuilder())
+            .UseLazyLoadingProxies()
+            .UseSqlServer(@"Data Source=45.119.212.145;Initial Catalog=MyPhamUsa_1;persist security info=True;Integrated Security=False;TrustServerCertificate=False;uid=sa;password=zaq@123;Trusted_Connection=False;MultipleActiveResultSets=true;")
+            .Options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
