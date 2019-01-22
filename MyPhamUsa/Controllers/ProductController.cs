117c3a724b9f1b0e5c823a9e92a4ff2875834f4e
phucit1025 <phucit1025@gmail.com>
2019-01-22 15:24:13 +0700

    .

--- MyPhamUsa/Controllers/ProductController.cs
+++ MyPhamUsa/Controllers/ProductController.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyPhamUsa.Services.Interfaces;
+using MyPhamUsa.Models.ViewModels;
 
 namespace MyPhamUsa.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Product/[action]")]
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] ProductCreateViewModel newProduct)
+        {
+            var result = _productService.CreateProduct(newProduct);
+            if (result)
+            {
+                return StatusCode(200);
+            }
+            else
+            {
+                return StatusCode(400);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var result = _productService.GetProducts();
+            return StatusCode(200, result);
+        }
     }
 }
